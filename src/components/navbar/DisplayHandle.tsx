2cceacdef090e357bab6abaf28f263d16734cb46
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-10-31 20:54:36 -0500

    add icon to handles

--- src/components/navbar/DisplayHandle.tsx
+++ src/components/navbar/DisplayHandle.tsx
@@ -13,6 +13,11 @@ export function DisplayHandle({display}: { display: AvailableDisplay }) {
         <div draggable={true} onDragStart={event => {
             event.dataTransfer.effectAllowed = "move";
             event.dataTransfer.setData("text/plain", display.id);
-        }} className={clsx("btn btn-neutral cursor-grab", display.used && "opacity-25")}>{display.name}</div>
+        }} className={clsx("btn btn-neutral cursor-grab flex items-center", display.used && "opacity-25")}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+            {display.name}
+        </div>
     )
 }
